88443ee4f21f0ce12f722ac6582c4548f80f594d
Hsuirad <34495439+Hsuirad@users.noreply.github.com>
2019-01-21 17:37:17 -0500

    Update parser.js

--- parser.js
+++ parser.js
@@ -1,8 +1,11 @@
 const puppeteer = require('puppeteer');
 var fs = require('fs');
 
+var max_iterations = 104;
+
 var text = fs.readFileSync('terms.txt', 'utf8');
-var words = text.split('\n', 10);
+var stream = fs.createWriteStream('parserdata');
+var words = text.split('\n', max_iterations);
 var count = 0;
 var dords = [];
 
@@ -17,55 +20,65 @@ for(var x = 0; x < words.length; x++){
 
 	}
 	dords[x] = words[x];
-	//console.log(count);
 	words[x] = str;
-	//console.log(str);
 }
 
-//console.log(words);
-
-let wordtouse = "";
 var testw = [];
+let data = "";
 
-for(var s = 0; s < words.length; s++){
-	scraper(s);
-}
+let p = 0;
+
+var iterations = 0;
+var interval = setInterval(function() {scraper(iterations);}, 4000);
 
 console.log('\n\n')
 
 function scraper(z){
-	wordtouse = words[z];
+
 	let scrape = async() =>  {
+
 		const browser = await puppeteer.launch({headless: true});
 		const page = await browser.newPage();
-		await page.goto(`https://www.google.com/search?q=${words[z]}&oq=${words[z]}&aqs=chrome..69i57j0l5.4903j0j7&sourceid=chrome&ie=UTF-8`);
-		await page.waitFor(3000);
+		await page.goto(`https://www.google.com/search?q=${words[z]}&oq=${words[z]}&aqs=chrome..69i57j0l5.4903j0j7&sourceid=chrome&ie=UTF-8`);	
+		await page.waitFor(1000);
+
+		const test = await page.evaluate(() => {
+			let test = document.querySelector('.ILfuVd').innerText;
+			return test;
+		});
+
 		kords = dords[z];
-
-
-		/*await page.click('#default > div > div > div > div > section > div:nth-child(2) > ol > li:nth-child(2) > article > div.image_container > a > img');
-		const result = await page.evaluate(() => {
-			let title = document.querySelector('h1').innerText;
-			let price = document.querySelector('.price_color').innerText;
-			return {
-				title,
-				price
-			}
-		});*/
 
 		const result = await page.evaluate(() => {
 			let definition = document.querySelector('.ILfuVd').innerText;
 			return definition;
 		});
+
 		console.log(kords + ":\n");
-		console.log(result + '\n');
-		console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ \n\n")
+		iterations++;
+
+		if(result){
+			console.log(result + '\n');
+		}
+		else{
+			console.log('-Definiton not found!-');
+		}
+
+		console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ \n\n");
+		
 		browser.close();
-		return result;
+		
+		return (kords + ":\n\n" + result + "\n\n\n"); 
 	}
 
 	scrape().then((value) => {
-		/*console.log(kords + ":\n");
-		console.log(value + '\n');*/
+
+		stream.write(value);
+		
+		if(iterations == max_iterations){
+			clearInterval(interval);
+		}
+		
+		p == max_iterations ? stream.end() : p++;
 	});
 }
